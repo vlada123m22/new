684c3b982ad2e78e56d7d88617b10171e78e3844
Vladislava Musin <135638760+vlada123m22@users.noreply.github.com>
2023-09-12 17:47:42 +0300

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Car {
+    float speed = 0;
+    boolean isMoving = false;
+
+    public void stop() {
+        isMoving = false;
+    }
+
+    public void accelerate(float amount) {
+        speed += amount;
+        isMoving = speed != 0;
+    }
+
+    public void printSpeed() {
+        System.out.println("Speed: " + speed);
+    }
+
+    public void printIsMoving() {
+        if (isMoving) {
+            System.out.println("The car is moving");
+        } else {
+            System.out.println("The car is not moving");
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.printIsMoving();
+
+    }
+}
